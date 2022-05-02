ae191daa01d3c8cce7ba56e0ffb38bca0aec50f6
godud2604 <ees238@naver.com>
2022-05-02 23:16:26 +0900

    Add home isLoading && next.config.js images domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['react.semantic-ui.com'],
+  },
 }
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,21 @@ import axios from 'axios';
 import Head from 'next/head'
 import Image from 'next/image'
 import { useEffect, useState } from 'react';
-import { Divider, Header } from 'semantic-ui-react';
+import { Dimmer, Divider, Header, Loader, Segment } from 'semantic-ui-react';
 import ItemList from '../src/component/ItemList';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   const [list, setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  
   const API_URL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+
   function getData() {
     axios.get(API_URL)
       .then((res) => {
         setList(res.data);
+        setIsLoading(false);
       }
     );
   }
@@ -21,10 +25,19 @@ export default function Home() {
     getData();
   }, []);
 
+  if (isLoading) {
+    return (
+      <div style={{ padding: "300px 0"}}>
+        <Loader inline="centered" active />
+      </div>
+    )
+  }
+
   return (
     <>
       <Head>
         <title>HOME</title>
+        <meta name="description" content="메이크업 쇼핑몰 입니다." ></meta>
       </Head>
       <Header as="h3" styles={{ paddingTop: 40 }}>
         베스트 상품
